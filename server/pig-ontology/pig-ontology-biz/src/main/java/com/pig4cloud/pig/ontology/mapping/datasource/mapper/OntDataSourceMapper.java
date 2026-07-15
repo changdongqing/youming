5bb372157d2cdd138af00a379bc46c91399c68fc
ttshang <chdq0306@qq.com>
2026-07-15 18:12:38 +0800

    bug修复

--- server/pig-ontology/pig-ontology-biz/src/main/java/com/pig4cloud/pig/ontology/mapping/datasource/mapper/OntDataSourceMapper.java
+++ server/pig-ontology/pig-ontology-biz/src/main/java/com/pig4cloud/pig/ontology/mapping/datasource/mapper/OntDataSourceMapper.java
@@ -6,12 +6,14 @@ package com.pig4cloud.pig.ontology.mapping.datasource.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pig4cloud.pig.ontology.mapping.datasource.entity.OntDataSource;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 数据源注册 Mapper。
  *
  * @author youming
  */
+@Mapper
 public interface OntDataSourceMapper extends BaseMapper<OntDataSource> {
 
 }
--- server/pig-ontology/pig-ontology-biz/src/main/java/com/pig4cloud/pig/ontology/mapping/datasource/mapper/OntDataSourceMetadataMapper.java
+++ server/pig-ontology/pig-ontology-biz/src/main/java/com/pig4cloud/pig/ontology/mapping/datasource/mapper/OntDataSourceMetadataMapper.java
@@ -6,12 +6,14 @@ package com.pig4cloud.pig.ontology.mapping.datasource.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pig4cloud.pig.ontology.mapping.datasource.entity.OntDataSourceMetadata;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 数据源元数据缓存 Mapper。
  *
  * @author youming
  */
+@Mapper
 public interface OntDataSourceMetadataMapper extends BaseMapper<OntDataSourceMetadata> {
 
 }
